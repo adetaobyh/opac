ed2cf752ca8561be38ce58e696e0a90061f57933
Danang Estutomoaji <ahmad.candra.17@gmail.com>
2023-01-11 00:35:46 +0700

    Add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+        'siswa' => \App\Http\Middleware\Siswa::class,
+        'guru' => \App\Http\Middleware\Guru::class,
+        'revalidate' => \App\Http\Middleware\RevalidateBackHistory::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -34,38 +34,49 @@ Route::post('/register', [RegisterController::class, 'store'])->name('register')
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 
-// Admin
-Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+Route::middleware(['auth', 'admin', 'revalidate'])->group(function () {
 
-// CRUD Catalog
-Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog');
-Route::get('/tambah-catalog', [CatalogController::class, 'create'])->name('tambah-catalog');
-Route::post('/simpan-catalog', [CatalogController::class, 'store'])->name('simpan-catalog');
-Route::get('/edit-catalog/{id}', [CatalogController::class, 'edit'])->name('edit-catalog');
-Route::post('/update-catalog/{id}', [CatalogController::class, 'update'])->name('update-catalog');
-Route::get('/hapus-catalog/{id}', [CatalogController::class, 'destroy'])->name('hapus-catalog');
+    // Admin
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+    
+    // CRUD Catalog
+    Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog');
+    Route::get('/tambah-catalog', [CatalogController::class, 'create'])->name('tambah-catalog');
+    Route::post('/simpan-catalog', [CatalogController::class, 'store'])->name('simpan-catalog');
+    Route::get('/edit-catalog/{id}', [CatalogController::class, 'edit'])->name('edit-catalog');
+    Route::post('/update-catalog/{id}', [CatalogController::class, 'update'])->name('update-catalog');
+    Route::get('/hapus-catalog/{id}', [CatalogController::class, 'destroy'])->name('hapus-catalog');
 
-// CRUD Book
-Route::get('/book', [BookController::class, 'index'])->name('book');
-Route::get('/tambah-book', [BookController::class, 'create'])->name('tambah-book');
-Route::post('/simpan-book', [BookController::class, 'store'])->name('simpan-book');
-Route::get('/edit-book/{id}', [BookController::class, 'edit'])->name('edit-book');
-Route::post('/update-book/{id}', [BookController::class, 'update'])->name('update-book');
-Route::get('/hapus-book/{id}', [BookController::class, 'destroy'])->name('hapus-book');
+    // CRUD Book
+    Route::get('/book', [BookController::class, 'index'])->name('book');
+    Route::get('/tambah-book', [BookController::class, 'create'])->name('tambah-book');
+    Route::post('/simpan-book', [BookController::class, 'store'])->name('simpan-book');
+    Route::get('/edit-book/{id}', [BookController::class, 'edit'])->name('edit-book');
+    Route::post('/update-book/{id}', [BookController::class, 'update'])->name('update-book');
+    Route::get('/hapus-book/{id}', [BookController::class, 'destroy'])->name('hapus-book');
 
-// CRUD User
-Route::get('/user', [DtUserController::class, 'index'])->name('user');
-Route::get('/tambah-user', [DtUserController::class, 'create'])->name('tambah-user');
-Route::post('/simpan-user', [DtUserController::class, 'store'])->name('simpan-user');
-Route::get('/edit-user/{id}', [DtUserController::class, 'edit'])->name('edit-user');
-Route::post('/update-user/{id}', [DtUserController::class, 'update'])->name('update-user');
-Route::get('/hapus-user/{id}', [DtUserController::class, 'destroy'])->name('hapus-user');
+    // CRUD User
+    Route::get('/user', [DtUserController::class, 'index'])->name('user');
+    Route::get('/tambah-user', [DtUserController::class, 'create'])->name('tambah-user');
+    Route::post('/simpan-user', [DtUserController::class, 'store'])->name('simpan-user');
+    Route::get('/edit-user/{id}', [DtUserController::class, 'edit'])->name('edit-user');
+    Route::post('/update-user/{id}', [DtUserController::class, 'update'])->name('update-user');
+    Route::get('/hapus-user/{id}', [DtUserController::class, 'destroy'])->name('hapus-user');
 
-
-// Pengawas
-Route::get('/pengawas', [PengawasController::class, 'index'])->name('pengawas');
+});
 
 
-// Siswa
-Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa');
+Route::middleware(['auth', 'guru', 'revalidate'])->group(function () {
+
+    // Pengawas
+    Route::get('/pengawas', [PengawasController::class, 'index'])->name('pengawas');
+
+});
+
+
+Route::middleware(['auth', 'siswa', 'revalidate'])->group(function () {
+    
+    // Siswa
+    Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa');
+});
 
